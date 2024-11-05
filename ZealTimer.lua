e18e429c8490218262fa91fd4e0220d9661d628d
Daniel Dollerup <d.dollerup@gmail.com>
2024-11-05 20:56:15 +0100

    Added a feature, when not moving the frame, we can click through the frame. Making the frame less likely to get in the way of targeting anything.

--- ZealTimer.lua
+++ ZealTimer.lua
@@ -4,6 +4,7 @@ zealTimerFrame:RegisterEvent("UNIT_AURA")
 zealTimerFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 zealTimerFrame:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 
+-- Set size and default start location
 zealTimerFrame:SetWidth(100)
 zealTimerFrame:SetHeight(90)
 zealTimerFrame:SetPoint("CENTER", UIParent, "CENTER", 145, 15) -- Moved 50 pixels to the right
@@ -20,6 +21,8 @@ zealTimerFrame:SetBackdrop({
 zealTimerFrame:SetBackdropColor(0, 0, 0, 0.7) -- Black with 70% transparency
 
 -- Enable dragging with CTRL + Left-Click
+zealTimerFrame.isMoving = false
+zealTimerFrame.isMouseEnabled = true
 zealTimerFrame:SetMovable(true)
 zealTimerFrame:EnableMouse(true)
 zealTimerFrame:RegisterForDrag("LeftButton")
@@ -27,13 +30,29 @@ zealTimerFrame:RegisterForDrag("LeftButton")
 zealTimerFrame:SetScript("OnMouseDown", function()
     if arg1 == "LeftButton" and IsControlKeyDown() then
         this:StartMoving()
+        this.isMoving = true
     end
 end)
 
 zealTimerFrame:SetScript("OnMouseUp", function()
-    if arg1 == "LeftButton" then
+    if this.isMoving then
         this:StopMovingOrSizing()
+        this.isMoving = false
     end
+end)
+
+zealTimerFrame:SetScript("OnEnter", function()
+    if not IsControlKeyDown() then
+        this:EnableMouse(false)
+        this.isMouseEnabled = false
+    else
+        this:EnableMouse(true)
+        this.isMouseEnabled = true
+    end
+end)
+
+zealTimerFrame:SetScript("OnLeave", function()
+    this:EnableMouse(true) -- Re-enable mouse after leaving to avoid getting "stuck"
 end)
 
 -- Create a font string to display the "Zeal:" label
@@ -50,8 +69,6 @@ zealTimerFrame.text:SetText("N/A")
 zealTimerFrame.rankText = zealTimerFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 zealTimerFrame.rankText:SetPoint("TOP", zealTimerFrame.text, "BOTTOM", 0, -5)
 zealTimerFrame.rankText:SetText("(N/A)")
-
-
 
 -- Known texture path for the Zeal buff icon
 local zealTexture = "Interface\\Icons\\INV_Jewelry_Talisman_01"
@@ -79,22 +96,6 @@ local function UpdateZealInfo()
     end
 end
 
--- Function to display remaining duration
--- local function DisplayZealDuration()
---     if zealStacks > 0 then
---         local elapsedTime = GetTime() - zealStartTime
---         local remainingTime = 30 - elapsedTime
---         if remainingTime > 0 then
---             print("Zeal Buff: " .. zealStacks .. " stacks, " .. string.format("%.1f", remainingTime) .. " seconds remaining.")
---         else
---             zealStacks = 0
---             print("Zeal Buff expired.")
---         end
---     else
---         print("Zeal Buff not active.")
---     end
--- end
-
 local function UpdateZealTimerGUI()
     local elapsedTime = GetTime() - zealStartTime
     local remainingTime = 30 - elapsedTime
@@ -106,26 +107,25 @@ local function UpdateZealTimerGUI()
     zealTimerFrame.rankText:SetText("(".. zealStacks ..")")
 end 
 
-zealTimerFrame:SetScript("OnUpdate", UpdateZealTimerGUI)
+zealTimerFrame:SetScript("OnUpdate", function() 
+    UpdateZealTimerGUI()
+    if not this.isMoving and not this.isMouseEnabled then
+        this:EnableMouse(true)
+        this.isMouseEnabled = true
+    end 
+end)
 
 -- Event handler
 zealTimerFrame:SetScript("OnEvent", function()
-    -- print("arg1: " .. arg1)
     if event == "UNIT_AURA" and arg1 == "player" then
         UpdateZealInfo()
-        -- DisplayZealDuration()
     elseif event == "PLAYER_ENTERING_WORLD" then
         UpdateZealInfo()
-        -- DisplayZealDuration()
     elseif event == "CHAT_MSG_SPELL_SELF_DAMAGE"  then
         if (string.find(arg1, "Crusader Strike")) then
             zealStartTime = GetTime()
-            -- DisplayZealDuration()
         end 
     end
 end)
-
-
-
 
 print("ZealTimer by Dollerp - Loaded")
